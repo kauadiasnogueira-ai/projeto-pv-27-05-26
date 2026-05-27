4fc4db45bd2788250240433957330526750cc7ad
kauadiasnogueira-ai <kaua.dias.nogueira@escola.pr.gov.br>
2026-05-27 16:44:37 -0300

    Refactor button click handling and update selectors

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
-const botoes = document.querySelectorAll(".botoes button");
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
-for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () {
-        for (let j = 0; j < botoes.length; j++) {
-            botoes[j].classList.remove('ativo');
+for(let i=0; i < botoes.length; i++){
+    botoes[i].onclick = function(){
+
+        for(let j=0; j < botoes.length; j++){
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
-        this.classList.add('ativo');
-    };
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2026-08-27T00:00:00");
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo1){
+let tempoAtual = new Date();
+let tempoFinal = tempoObjetivo1 - tempoAtual;
+let segundos = Math.floor(tempoFinal/1000);
+let minutos = Math.floor(segundos/60);
+let horas = Math.floor(minutos/60);
+let dias = Math.floor(horas/24);
+
+segundos = segundos % 60;
+minutos = minutos % 60;
+horas = horas % 24;
+return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+}
+
